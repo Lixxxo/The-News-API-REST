855f5ece742cb85994b819b7226ccb66038d26fe
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2021-12-03 23:49:28 -0300

    feat(news): create main method

--- src/main/java/cl/ucn/disc/dsm/lrojas/newsapi/TheNewsApiApplication.java
+++ src/main/java/cl/ucn/disc/dsm/lrojas/newsapi/TheNewsApiApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TheNewsApiApplication {
 
+	/**
+	 * The Main starting point.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TheNewsApiApplication.class, args);
 	}
