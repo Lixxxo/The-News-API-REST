68f07afb4da23fd1f53fba0acdf6d6add23bee6c
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2021-12-21 13:26:12 -0300

    feat: declare Bean to populate the database

--- src/main/java/cl/ucn/disc/dsm/lrojas/newsapi/TheNewsApiApplication.java
+++ src/main/java/cl/ucn/disc/dsm/lrojas/newsapi/TheNewsApiApplication.java
@@ -1,10 +1,19 @@
 package cl.ucn.disc.dsm.lrojas.newsapi;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TheNewsApiApplication {
+
+	/*** The {@link NewsRepository} used to initialize the database
+	 *
+	 */
+	@Autowired
+	private NewsRepository newsRepository;
 
 	/**
 	 * The Main starting point.
@@ -13,4 +22,14 @@ public class TheNewsApiApplication {
 		SpringApplication.run(TheNewsApiApplication.class, args);
 	}
 
+	/**
+	 * Initialize the data inside the Database
+	 * @return the data to use
+	 */
+	@Bean
+	protected InitializingBean initializingDatabase(){
+		return () -> {
+
+		};
+	}
 }
